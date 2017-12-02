473b4a1764614f08e84dc9cb4aaa9b441f8afe35
dvpitakov <dvpitakov@gmail.com>
2017-12-03 00:19:09 +0300

    сокращение кода

--- Kurs.asm
+++ Kurs.asm
@@ -1,12 +1,3 @@
-;**********************************************************************************************
-;MUL16
-;DIVIDE
-;ADD_16_16_16
-;INT16_TO_TIME_STRING
-;CURSOR_BACK
-;L0 temp2 - хронит состаяние программы
-; 
-;**********************************************************************************************
 .include "m8515def.inc" ;oaee ii?aaaeaiee ATmega8515 
 
 .equ SECUND = 1
@@ -64,7 +55,7 @@ PUSHA:
 	push XL
 	ret
 
-POPA:
+POPALL:
 	pop XL
 	pop XH
 	pop temp7
@@ -119,28 +110,22 @@ INIT_INTERRUPT:
 	out PORTD, temp 
 	ldi temp,(1<<INT0) 
 	out GICR,temp 
-
 	ser temp 
 	out DDRB, temp 
 	out PORTB, temp 
-
 	ser temp 
 	out DDRC, temp 
 	clr temp 
 	out PORTC, temp 
-
-;Iano?ieea oaeia?a 
 	ldi temp, 0xFF -4 -8 -16 -32
 	out TIMSK, temp 
 	ldi temp, 0x00 
 	out TCNT1H, temp 
 	out TCNT1L, temp 
-
 	ldi temp, HIGH(FREQUANCY / FREQUANCY_DIV)
 	out OCR1AH, temp 
 	ldi temp, LOW(FREQUANCY / FREQUANCY_DIV)
 	out OCR1AL, temp 
-
 	rcall INIT_DISPLAY 
 	ldi ZL, LOW(SELECT_TIME_MODE << 1) 
 	ldi ZH, HIGH(SELECT_TIME_MODE << 1) 
@@ -150,17 +135,13 @@ INIT_INTERRUPT:
 	ldi ZH, HIGH(MEM_START) 
 	ldi temp, 0x01 
 	rcall INIT_VARIABLES
-
-
 	ldi temp, 3
 	rcall DRAW_CURRENT_TIME
-
 	ldi temp0, 0x00 
 	ldi temp2, 0x01
 	ldi temp3, 0x03
 	ldi temp4, 0x00
 	ldi temp5, 0x00 
-
 	sei 
 
 
@@ -173,13 +154,9 @@ LOOP:
 TIMER_A_INTERRUPT:
 	push temp2
 	ldi temp, 0x00
-
 	sbrc temp2, 3
 	rjmp stop_counting_force
-
-
 	ldi temp, 0x03
-
 	pop temp2
 	push temp2
 	sbrc temp2, 7
@@ -190,40 +167,36 @@ TIMER_A_INTERRUPT:
 	ldi temp, 6
 	sbrc temp2, 4
 	ldi temp, 7
+	ldi ZL, LOW(MEM_START)
+	ldi ZH, HIGH(MEM_START)
+	ldi temp5, HIGH(STRUCT_LEN)
+	ldi temp6, LOW(STRUCT_LEN)
+	ldi temp7, 0x09
 
-	ldi ZH, HIGH(0 * STRUCT_LEN + MEM_START) 
-	ldi ZL, LOW(0 * STRUCT_LEN + MEM_START) 
+adding_circle:
+	dec temp7
+	breq adding_end2
 	rcall ADD16
+	add ZL, temp6
+	adc ZH, temp5
+	rjmp adding_circle
 
-	ldi ZH, HIGH(1 * STRUCT_LEN + MEM_START) 
-	ldi ZL, LOW(1 * STRUCT_LEN + MEM_START) 
-	rcall ADD16 
-
-	ldi ZH, HIGH(2 * STRUCT_LEN + MEM_START) 
-	ldi ZL, LOW(2 * STRUCT_LEN + MEM_START) 
-	rcall ADD16 
-
-	ldi ZH, HIGH(5 * STRUCT_LEN + MEM_START) 
-	ldi ZL, LOW(5 * STRUCT_LEN + MEM_START) 
-	rcall ADD16
-
-	ldi ZH, HIGH(6 * STRUCT_LEN + MEM_START) 
-	ldi ZL, LOW(6 * STRUCT_LEN + MEM_START) 
-	rcall ADD16 
-
-	ldi ZH, HIGH(7 * STRUCT_LEN + MEM_START) 
-	ldi ZL, LOW(7 * STRUCT_LEN + MEM_START) 
-	rcall ADD16 
-
-	ldi ZH, HIGH(3 * STRUCT_LEN + MEM_START) 
-	ldi ZL, LOW(3 * STRUCT_LEN + MEM_START) 
-	rcall MINUS16
+adding_end2:
+	ldi ZL, LOW(MEM_START + 3 * STRUCT_LEN)
+	ldi ZH, HIGH(MEM_START + 3 * STRUCT_LEN)
+	ldd temp6, Z+1
+	tst temp6
+	brne go_continue_counting
+	ldd temp6, Z+2
+	tst temp6
+	brne go_continue_counting
+	ldd temp6, Z+3
+	tst temp6
 	breq stop_counting
-
+go_continue_counting:
 	rcall DRAW_CURRENT_TIME
 	pop temp2
 	reti 
-
 
 stop_counting:
 	pop temp2;O_o
@@ -239,63 +212,47 @@ stop_counting_force:
 	pop temp2;O_o
 	reti
 
-
-
 ;Z012 - первое число и результат
 ;Z345 - второе число
 ;Zx старше Zx+1
-
-ADD24:
-	rcall PUSHA
+ADD_MINUS_CORE:
 	ldd temp1, Z+0 
 	ldd temp2, Z+1 
 	ldd temp3, Z+2 
 	ldd temp4, Z+3 
 	ldd temp5, Z+4
 	ldd temp6, Z+5
-	add temp3, temp6 
-	adc temp2, temp5
-	adc temp1, temp4
-	std Z+0, temp1 
-	std Z+1, temp2 
-	std Z+2, temp3 
-	rcall POPA
-	ret 
-
-MINUS24:
-	rcall PUSHA
-	ldd temp1, Z+0 
-	ldd temp2, Z+1 
-	ldd temp3, Z+2 
-	ldd temp4, Z+3 
-	ldd temp5, Z+4
-	ldd temp6, Z+5
-
+	tst temp7
+	brne add_minus_core_prop
 	com temp4
 	com temp5
 	com temp6
-
 	inc temp6
 	adc temp5, r14
 	adc temp4, r14
 
+add_minus_core_prop:
 	add temp3, temp6 
 	adc temp2, temp5
 	adc temp1, temp4
 	std Z+0, temp1 
 	std Z+1, temp2 
 	std Z+2, temp3 
-	rcall POPA
+	rcall POPALL
 	ret 
 
-
-
+ADD24:
+	rcall PUSHA
+	ldi temp7, 0x01
+	rjmp ADD_MINUS_CORE
 
 ADD16: 
 	ldd temp1, Z+0 
 	ldd temp2, Z+1 
 	ldd temp3, Z+2
 	ldd temp4, Z+3
+	tst temp1
+	brmi if_minus
 	add temp4, temp1 
 	adc temp3, r14
 	adc temp2, r14
@@ -304,23 +261,7 @@ ADD16_END:
 	std Z+1, temp2
 	std Z+2, temp3 
 	std Z+3, temp4 
-	ret 
-
-MINUS16: 
-	ldd temp1, Z+0 
-	ldd temp2, Z+1 
-	ldd temp3, Z+2
-	ldd temp4, Z+3
-	com temp1
-	inc temp1
-	add temp4, temp1 
-	adc temp3, r15
-	adc temp2, r15
-	std Z+1, temp2 
-	std Z+2, temp3 
-	std Z+3, temp4
-
-
+add_16_cont:
 	tst temp2
 	brne r2253
 	tst temp3
@@ -329,12 +270,18 @@ MINUS16:
 r2253:
 	ret 
 
+if_minus: 
+	add temp4, temp1 
+	adc temp3, r15
+	adc temp2, r15
+	std Z+1, temp2 
+	std Z+2, temp3 
+	std Z+3, temp4
+	rjmp add_16_cont
+
 ;temp from what
 DRAW_PORTION:
 	rcall PUSHA
-	rcall TROLLEY_TIMER012
-	ldi ZH,HIGH(9 * STRUCT_LEN + MEM_START)
-	ldi ZL,LOW(9 * STRUCT_LEN + MEM_START)
 	rcall ADD_16_16_16
 	ldd temp1,Z+0
 	ldd temp2,Z+1
@@ -370,7 +317,7 @@ draw_propotion_adding_end:
 	rcall FLOAT_16_TO_STR 
 	ldi temp5, WRITE_IN_BOTTOM
 	rcall DRAW2
-	rcall POPA
+	rcall POPALL
 	ret
 ;temp from what
 
@@ -401,7 +348,7 @@ draw_current_time_adding_end:
 	rcall INT24_TO_TIME_STRING 
 	ldi temp5,  WRITE_IN_BOTTOM
 	rcall DRAW2
-	rcall POPA
+	rcall POPALL
 	ret
 
  
@@ -411,7 +358,7 @@ INIT_VARIABLES:
 	push temp1
 	push ZL
 	push ZH
-	ldi temp1, 3
+	ldi temp1, 10
 init_variables_loop:
 	ldi temp, 1 
 	st Z+, temp 
@@ -422,37 +369,23 @@ init_variables_loop:
 	dec temp1
 	brne init_variables_loop
 
-	ldi temp, 1
-	st Z+, temp 
-	ldi temp, 0
-	st Z+, temp 
-	ldi temp, HIGH(HOUR * 3) 
-	st Z+, temp 
-	ldi temp, LOW(HOUR * 3) 
-	st Z+, temp 
-
-	ldi temp1,6
-init_variables_loop2:
-	ldi temp, 1
-	st Z+, temp
-	ldi temp, 0
-	st Z+, temp 
-	st Z+, temp
-	st Z+, temp
-	dec temp1
-	brne init_variables_loop2
-
 	pop ZH
 	pop ZL
+
+	ldi temp, -1
+	std Z + 3 * STRUCT_LEN, temp 
+	ldi temp, 0
+	std Z + 3 * STRUCT_LEN + 1, temp 
+	ldi temp, HIGH(HOUR * 3) 
+	std Z + 3 * STRUCT_LEN + 2, temp 
+	ldi temp, LOW(HOUR * 3) 
+	std Z + 3 * STRUCT_LEN + 3, temp 
+
 	pop temp1
 	pop temp
 	pop temp4
 	ret 
 
-;temp - used 
-;temp1 - used - нажаиая кнопка
-;temp2 - state 
-;temp1?
 ON_BUTTON_PRESSED_INTERRUPT:
 	ldi temp5, 0x03
 	ldi temp6, 0x00
@@ -469,8 +402,6 @@ ON_BUTTON_PRESSED_INTERRUPT:
 	sbrs temp1, 7 
 	rjmp ON_BUTTON7_PRESSED
 
-
-;aee??aai oaeia?u 
 ON_BUTTON3_PRESSED:
 	sbrc temp2, TIME_INTERVAL_STATE
 	rcall on_button3_pressed_start_timing
@@ -482,7 +413,6 @@ ON_BUTTON3_PRESSED:
 	rcall on_time_seup_state_start 
 	reti
 
-;TODO
 on_time_seup_state_start:
 	inc temp4
 	ret
@@ -544,7 +474,6 @@ rr_t1:
 rr_t3:
 	push ZH
 	push ZL
-
 	tst temp6
 	brne tg1
 	ldi temp1, HIGH(HOUR)
@@ -566,11 +495,9 @@ tg3:
 	ldd temp5, Z+3
 	ldi ZH, HIGH(8 * STRUCT_LEN + MEM_START)
 	ldi ZL, LOW(8 * STRUCT_LEN + MEM_START)
-
 	std Z+0, temp3
 	std Z+1, temp4
 	std Z+2, temp5
-
 	push temp0
 	ldi temp, 0x00
 	std Z+3, temp0
@@ -590,11 +517,8 @@ tg3:
 	ldi temp, 0x05
 	add temp, temp5
 	rcall DRAW_CURRENT_TIME
-	rcall POPA
+	rcall POPALL
 	ret
-
-
-
 
 on_button4_pressed_set_1hour_interval:
 	mov temp, temp5
@@ -615,8 +539,6 @@ on_button4_out_proportion:
 	rcall DRAW_PORTION
 	ret
 
-
-
 ON_BUTTON7_PRESSED:
 	sbrc temp2, OUT_RESULT_STATE
 	rcall on_button7_pressed_get_sum
@@ -629,9 +551,6 @@ ON_BUTTON7_PRESSED:
 	reti
 on_button7_pressed_get_sum:
 	push temp2
-	rcall TROLLEY_TIMER012
-	ldi ZL, LOW(9 * STRUCT_LEN + MEM_START)
-	ldi ZH, HIGH(9 * STRUCT_LEN + MEM_START)
 	rcall ADD_16_16_16
 	push temp0
 	push temp1
@@ -722,105 +641,50 @@ adding_end:
 	pop ZH
 	pop ZL
 	ret
-;temp1-HByte
-;temp2-LByte
-;temp3-HByte
-;temp4-LByte
-;temp1-OutHByte
-;temp2-OutLByte
-
 
 DIFERENCE24:
-	push temp3
-	push temp4
-	push temp5
-
-
-	add temp5, r15
-	adc temp4, r15 
-	adc temp3, r15
-	 
-    com temp5
-	com temp4 
-	com temp3 
-
+    com temp2
+	com temp1
+	com temp0 
 	add temp2, temp5
 	adc temp1, temp4 
 	adc temp0, temp3
-
-	pop temp5
-	pop temp4
-	pop temp3
+	com temp2
+	com temp1
+	com temp0
 	ret 
 	
-SUM24_16: 
-	push temp8
-	ldi temp8, 0x00
-	add temp2, temp4 
-	adc temp1, temp3
-	adc temp0, temp8
-	pop temp8
-	ret 
-
-
-
-MUL2_24:
-	push temp1
-	push temp2
-	push temp3
-
-	ldd temp1, Z+0
-	ldd temp2, Z+1
-	ldd temp3, Z+2
-
-	lsl temp3
-	rol temp2
-	rol temp1
-
-	std Z+0, temp1
-	std Z+1, temp2
-	std Z+2, temp3
-
-	pop temp3
-	pop temp2
-	pop temp1
-	ret
-
 
 FLOAT_16_TO_STR:
 	rcall PUSHA
-
 	ldd temp7, Z+4
 	ldd temp8, Z+5
 	std Z+0, temp7
 	std Z+1, temp8
 	ldi temp5, 0x04
 float_16_to_str_loop:
-		ldi temp1, 0x00
-		ldi temp2, 0x0A
-		std Z+2, temp1
-		std Z+3, temp2
-		rcall MUL16
-		ldd temp1, Z+2
-		ldd temp2, Z+3
-		std Z+0, temp1
-		std Z+1, temp2
-		ldd temp, Z+4
-		push temp
-		dec temp5
+	ldi temp1, 0x00
+	ldi temp2, 0x0A
+	std Z+2, temp1
+	std Z+3, temp2
+	rcall MUL16
+	ldd temp1, Z+2
+	ldd temp2, Z+3
+	std Z+0, temp1
+	std Z+1, temp2
+	ldd temp, Z+4
+	push temp
+	dec temp5
 	brne float_16_to_str_loop
-
 	pop temp1
 	pop temp2
 	pop temp3
 	pop temp4
-
 	ldi temp, '0'
 	add temp1, temp
 	add temp2, temp
 	add temp3, temp
 	add temp4, temp
-
 	std Z+0, temp4
 	std Z+1, temp3
 	ldi temp, ','
@@ -835,31 +699,42 @@ float_16_to_str_loop:
 	std Z+8, temp
 	ldi temp, 0
 	std Z+9, temp
-	rcall POPA
+	rcall POPALL
 	ret
+
 
 DIVIDE: 
 	rcall PUSHA
 	ldi temp, 40
 	std Z+6, temp
-
 	ldi temp0, 0x00
 	ldi temp1, 0x00
 	ldi temp2, 0x00
-
 	ldd temp3, Z+3
 	ldd temp4, Z+4
 	ldi temp5, 0x00
 	ldi temp6, 0x00
 	ldi temp7, 0x00
 	ldi temp8, 0x00
-
 divide_circle:
-	rcall MUL2_24
+	push temp1
+	push temp2
+	push temp3
+	ldd temp1, Z+0
+	ldd temp2, Z+1
+	ldd temp3, Z+2
+	lsl temp3
+	rol temp2
+	rol temp1
+	std Z+0, temp1
+	std Z+1, temp2
+	std Z+2, temp3
+	pop temp3
+	pop temp2
+	pop temp1
 	rol temp2
 	rol temp1
 	rol temp0
-
 	push temp5
 	push temp4
 	push temp3
@@ -870,7 +745,7 @@ divide_circle:
 	pop temp3
 	pop temp4
 	pop temp5 
-	brcc recov
+	brmi recov
 	adc temp8, r14
 divide_circle_tail:
 	std Z+7, temp
@@ -889,18 +764,21 @@ divide_circle_end:
 	std Z+1, temp6
 	std Z+4, temp7
 	std Z+5, temp8
-	rcall POPA 
-
+	rcall POPALL
 	ret 
 recov: 
-	rcall SUM24_16
+	push temp8
+	ldi temp8, 0x00
+	add temp2, temp4 
+	adc temp1, temp3
+	adc temp0, temp8
+	pop temp8
 	rjmp divide_circle_tail
 
 ;!!!сохраняет результаты в Z+2 Z+3 Z+4!!!
 ;!!!Z+4 OVERFLOW
 MUL16:
 	rcall PUSHA
-
 	ldd temp1, Z+0
 	ldd temp2, Z+1
 	ldd temp3, Z+2
@@ -916,18 +794,15 @@ mul16_loop:
 mul16_con:
 	dec temp
 	breq mul16_end
-
 	lsl temp6
 	rol temp5
 	rol temp7
-	
 	rjmp mul16_loop
 mul16_end:
 	std Z+2, temp5
 	std Z+3, temp6
 	std Z+4, temp7
-
-	rcall POPA
+	rcall POPALL
 	ret
 mul16_add:
 	add temp6, temp2
@@ -936,41 +811,25 @@ mul16_add:
 	inc temp7
 	rjmp mul16_con
 	
-;Register call function
-;r1,2,3,4,5,6 - data
-;r1,2 - sum
 ADD_16_16_16:
+	rcall TROLLEY_TIMER012
+	ldi ZH,HIGH(9 * STRUCT_LEN + MEM_START)
+	ldi ZL,LOW(9 * STRUCT_LEN + MEM_START)
 	rcall PUSHA
- 
 	ldd temp1, Z+0 
 	ldd temp2, Z+1 
 	ldd temp3, Z+2 
 	ldd temp4, Z+3 
 	ldd temp5, Z+4
 	ldd temp6, Z+5
-
 	add temp2, temp4 
 	adc temp1, temp3  
- 
 	add temp2, temp6 
 	adc temp1, temp5
-
 	std Z+0, temp1
 	std Z+1, temp2
-
-	rcall POPA
+	rcall POPALL
 	ret 
-
-OUT_RESULT: 
-ret 
-
-OUT_ADD_RESULT: 
-ret 
-;used registers
-;old;Z+0-HIGH 
-;old;Z+1-LOW
-;...-used 
-;Z+X-out data str 
 
 INT24_TO_TIME_STRING:
 	rcall PUSHA
@@ -979,7 +838,6 @@ INT24_TO_TIME_STRING:
 	ldd temp0, Z+0
 	ldd temp1, Z+1
 	ldd temp2, Z+2
-
 	ldi temp3, 0x00
 	ldi temp4, 0x08
 	mov r3, temp3
@@ -988,7 +846,6 @@ INT24_TO_TIME_STRING:
 	mov r5, ZH
 	add r4, ZL
 	adc r3, ZH
-
 	ldi temp4, HIGH(60 * 60)
 	ldi temp5, LOW(60 * 60)
 	rcall INT_24_TO_STRING_CORE
@@ -998,13 +855,11 @@ INT24_TO_TIME_STRING:
 	ldi temp4, HIGH(1)
 	ldi temp5, LOW(1)
 	rcall INT_24_TO_STRING_CORE
-
 	pop ZL
 	pop ZH
 	ldi temp, 0x00
 	std Z+8, temp
-
-	rcall POPA
+	rcall POPALL
 	ret 
 
 INT_24_TO_STRING_CORE:
@@ -1025,14 +880,11 @@ INT_24_TO_STRING_CORE:
 	adc ZH, r14
 	add ZL, temp3
 	adc ZH, r14
-
-
 	push temp1
 	push temp2
 	push temp
 	push ZH
 	push ZL
-
 	lpm temp1, Z+0
 	lpm temp2, Z+1
 	ldi temp, ':'
@@ -1043,14 +895,11 @@ INT_24_TO_STRING_CORE:
 	st Z+, temp
 	mov r5, ZH
 	mov r6, ZL
-
 	pop ZL
 	pop ZH
 	pop temp
 	pop temp2
 	pop temp1
-
-
 	mov ZL, r4
 	mov ZH, r3
 	std Z+2, temp4
@@ -1096,13 +945,10 @@ INT_24_TO_STRING_CORE:
 INIT_DISPLAY: 
 	rcall PUSHA
 	rcall WAIT 
-
 	ldi temp0, 0 
 	ldi temp1, 1 
 	ldi temp2, 2 
 	ldi temp3, 3 
-
- 
 	ldi temp4, SET_WIND_PARAMS | SET_8_BIT | SET_2_LINES 
 	rcall SEND_BYTE_COMAND
 	ldi temp4,ACTIVATE_DISPLAY 
@@ -1113,8 +959,7 @@ INIT_DISPLAY:
 	rcall SEND_BYTE_COMAND
 	ldi temp4, ACTIVATE_DISPLAY | ACTIVATE_ACTIVATE 
 	rcall SEND_BYTE_COMAND
-
-	rcall POPA
+	rcall POPALL
 	ret 
 
 SEND_BYTE_COMAND:
@@ -1150,19 +995,13 @@ DRAW:
 
 
 DRAW2: 
-	push temp0 
-	push temp1 
-	push temp2 
-	push temp3 
-	push temp6
+	rcall PUSHA
 	push ZH
 	push ZL 
-
 	ldi temp0, 0 
 	ldi temp1, 1 
 	ldi temp2, 2 
 	ldi temp3, 3 
-
 	out PORTC, temp0 
 	out PORTC, temp2 
 	rcall WAIT_SMALL 
@@ -1186,15 +1025,10 @@ draw2_str:
 draw2_end:
 	pop ZL
 	pop ZH 
-	pop temp6 
-	pop temp3 
-	pop temp2 
-	pop temp1 
-	pop temp0 
+	rcall POPALL
 	ret 
 
 WAIT: 
-
 	push temp1 
 	push temp2 
 	push temp3 
@@ -1211,7 +1045,6 @@ wait_loop3:
 	inc temp3 
 	breq wait_loop2 
 	rjmp wait_loop3 
-
 wait_end: 
 	pop temp3 
 	pop temp2 
@@ -1220,7 +1053,6 @@ wait_end:
 
 
 WAIT_SMALL: 
-
 	push temp1 
 	push temp2 
 	push temp3 
@@ -1243,6 +1075,7 @@ wait_end1:
 	pop temp2 
 	pop temp1 
 	ret 
+
 TIME_SET:
 .db "00010203040506070809"
 .db "10111213141516171819"
